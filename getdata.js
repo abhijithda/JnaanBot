25b3f97a598d6ee1c89994d2dfeffe9f1be309eb
Abhijith D.A <abhijith.da@gmail.com>
2018-07-28 19:05:03 -0700

    Remove local json data file and the func that was reading that file.

--- getdata.js
+++ getdata.js
@@ -2,14 +2,6 @@
 const fetch = require('node-fetch');
 
 module.exports = {
-    getLocalData: function (topic) {
-        var json = require('./data/data.json');
-        // console.log(json)
-        console.log("Getting data for ...", topic)
-        console.log(json[topic])
-        return json[topic]
-    },
-
     getData: async function (url, topic) {
         // read JSON from URL
         let response = await fetch(url);
